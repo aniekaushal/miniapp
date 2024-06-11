b708915bc3ade980910a926279ede243f654d3c1
aniekaushal <anie.kaushal@gmail.com>
2024-06-11 20:31:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function handleCredentialResponse(response) {
+    const data = jwt_decode(response.credential);
+    console.log("ID: " + data.sub);
+    console.log('Full Name: ' + data.name);
+    console.log('Given Name: ' + data.given_name);
+    console.log('Family Name: ' + data.family_name);
+    console.log("Image URL: " + data.picture);
+    console.log("Email: " + data.email);
+}
+
+window.onload = function () {
+    google.accounts.id.initialize({
+        client_id: "409306020032-rvmvr5n9cua04mqceb19fq4grn4ga2s4.apps.googleusercontent.com",
+        callback: handleCredentialResponse
+    });
+    google.accounts.id.renderButton(
+        document.getElementById("buttonDiv"),
+        { theme: "outline", size: "large" }  // customization attributes
+    );
+    google.accounts.id.prompt(); // also display the One Tap dialog
+}
