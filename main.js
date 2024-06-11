a8ddfd39f2caa09d6bfedbcc7e2a6838f3a15eed
aniekaushal <anie.kaushal@gmail.com>
2024-06-11 22:25:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,33 @@
 function decodeJWT(token) {
-    // Helper function to convert a base64-encoded string to a Uint8Array
     function base64ToUint8Array(base64) {
-      const binaryString = atob(base64);
-      const bytes = new Uint8Array(binaryString.length);
-      for (let i = 0; i < binaryString.length; i++) {
-        bytes[i] = binaryString.charCodeAt(i);
-      }
-      return bytes;
+        const binaryString = atob(base64);
+        const bytes = new Uint8Array(binaryString.length);
+        for (let i = 0; i < binaryString.length; i++) {
+            bytes[i] = binaryString.charCodeAt(i);
+        }
+        return bytes;
     }
-  
-    // Helper function to convert a Uint8Array to a string
+
     function uint8ArrayToString(uint8Array) {
-      return String.fromCharCode.apply(null, uint8Array);
+        return String.fromCharCode.apply(null, uint8Array);
     }
-  
-    // Split the token into three parts (header, payload, and signature)
+
     const [header, payload, signature] = token.split('.');
-  
-    // Decode the base64-encoded header and payload
+
     const decodedHeader = uint8ArrayToString(base64ToUint8Array(header));
     const decodedPayload = uint8ArrayToString(base64ToUint8Array(payload));
-  
-    // Parse the decoded header and payload as JSON objects
+
     const parsedHeader = JSON.parse(decodedHeader);
     const parsedPayload = JSON.parse(decodedPayload);
-  
-    // Return an object containing the header and payload
+
     return {
-      header: parsedHeader,
-      payload: parsedPayload,
+        header: parsedHeader,
+        payload: parsedPayload,
     };
-  }
+}
 
 function handleCredentialResponse(response) {
-    const data = decodeJWT(response.credential).payload
+    const data = decodeJWT(response.credential).payload;
     displayUserInfo(data);
 }
 
@@ -58,11 +52,12 @@ function logout() {
 window.onload = function () {
     google.accounts.id.initialize({
         client_id: "409306020032-rvmvr5n9cua04mqceb19fq4grn4ga2s4.apps.googleusercontent.com",
-        callback: handleCredentialResponse
+        callback: handleCredentialResponse,
+        ux_mode: "popup"  // Use popup mode instead of redirect
     });
     google.accounts.id.renderButton(
         document.getElementById("buttonDiv"),
-        { theme: "outline", size: "large" }  // customization attributes
+        { theme: "outline", size: "large" }
     );
-    google.accounts.id.prompt(); // also display the One Tap dialog
+    google.accounts.id.prompt();
 }
